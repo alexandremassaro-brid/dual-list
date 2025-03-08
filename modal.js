53630ada9ee5d841c56df3d9209fb6de02e9d762
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-08 11:33:34 -0300

    Criado construtor para a classe Modal

--- modal.js
+++ modal.js
@@ -3,4 +3,10 @@
  * @description A dialog modal that opens over the web page.
  */
 export class Modal {
+    /**
+     * @constructor
+     */
+    constructor() {
+        // Initialize Element
+    }
 }
