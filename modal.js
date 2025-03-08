0dbb2d632d383d0eecce8d17eef55428215606c9
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-08 13:59:18 -0300

    Corrections to the Moadal class

--- modal.js
+++ modal.js
@@ -1,38 +1,53 @@
 /**
- * @class Modal
- * @description A dialog modal that opens over the web page.
+ * @classdesc A dialog modal that opens over the web page.
+ * @class
+ * @public
  */
 export class Modal {
     /**
+     * The Html component used to render the modal to the screen.
      * @private
+     * @constant
      * @type {HTMLDivElement}
-     * @description The Html component used to render the modal to the screen.
      */
     #htmlElement;
 
     /**
-     * @constructor
+     * Returns the modal id
+     * @property {string} - The HTML element id.
+     * @public
+     */
+    get id() {
+        console.log(this.#htmlElement.id);
+        return this.#htmlElement.id;
+    }
+
+    /**
+     * Class constructor - Returns a Modal instance.
      */
     constructor() {
         this.#initializeElement();
     }
 
     /**
-     * Initializes the HTMLElement
+     * Initializes the HTMLElement.
      * @private
+     * @method
+     * @param {string} [id='dialogModal'] - The id to be used in this modal.
+     * @param {string} [label='Dialog Modal'] - The text to be used in the modal's title.
     */
-   #initializeElement(id, label) {
+   #initializeElement(id = 'dialogModal', label = 'Dialog Modal') {
         // Create modal div tag
         const requiredClasses = [
             'modal',
             'fade',
         ];
-        const modalId = 'dialogModal';
+        const modalId = id;
         const tabIndex = '-1';
         const role = 'dialog';
         const ariaLabelledBy = 'modalTitleLabel';
 
-        this.#htmlElement = new HTMLDivElement();
+        this.#htmlElement = document.createElement('div');
         this.#htmlElement.classList.add(requiredClasses);
         this.#htmlElement.id = modalId;
         this.#htmlElement.tabIndex = tabIndex;
@@ -45,7 +60,7 @@ export class Modal {
         ];
         const documentRole = 'document';
 
-        const documentDiv = new HTMLDivElement();
+        const documentDiv = document.createElement('div');
         documentDiv.classList.add(documentRequiredClasses);
         documentDiv.role = documentRole;
 
@@ -56,6 +71,8 @@ export class Modal {
 
     /**
      * Returns an HTMLElement object to be appended to the HTML Document
+     * @method
+     * @public
      * @returns {HTMLDivElement} The Modal's HTMLElement
     */
     render() {
