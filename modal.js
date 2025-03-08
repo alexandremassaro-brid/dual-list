d1cbf2ced76d7c8d9b99555dde1201b501d0812d
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-08 12:04:24 -0300

    Adicionado atributo privado htmlElement a classe Modal. Inicializada a tag div do modal.

--- modal.js
+++ modal.js
@@ -4,6 +4,13 @@
  */
 export class Modal {
     /**
+     * @private
+     * @type {HTMLDivElement}
+     * @description The Html component used to render the modal to the screen.
+     */
+    #htmlElement;
+
+    /**
      * @constructor
      */
     constructor() {
@@ -15,6 +22,24 @@ export class Modal {
      * @private
     */
    #initializeElement(id, label) {
-    // Initialize element
-}
+        // Create modal div tag
+        const requiredClasses = [
+            'modal',
+            'fade',
+        ];
+        const modalId = 'dialogModal';
+        const tabIndex = '-1';
+        const role = 'dialog';
+        const ariaLabelledBy = 'modalTitleLabel';
+
+        this.#htmlElement = new HTMLDivElement();
+        this.#htmlElement.classList.add(requiredClasses);
+        this.#htmlElement.id = modalId;
+        this.#htmlElement.tabIndex = tabIndex;
+        this.#htmlElement.role = role;
+        this.#htmlElement.setAttribute('aria-labbeledby', ariaLabelledBy);
+
+        // Create document div tag
+        // Initialize Content
+    }
 }
