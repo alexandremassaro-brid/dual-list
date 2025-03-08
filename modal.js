ee107f31fba73c564c3b49f7b8b319df2f3d02f2
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-08 15:24:56 -0300

    Implementado o método que inicializa o cabeçalho

--- modal.js
+++ modal.js
@@ -13,6 +13,22 @@ export class Modal {
     #htmlElement;
 
     /**
+     * The Html component used to render the modal's content.
+     * @private
+     * @constant
+     * @type {HTMLDivElement}
+     */
+    #contentHtmlElement;
+
+    /**
+     * The Html component used to render the modal's header.
+     * @private
+     * @constant
+     * @type {HTMLDivElement}
+     */
+    #headerHtmlElement;
+
+    /**
      * Returns the modal id
      * @property {string} - The HTML element id.
      * @public
@@ -93,20 +109,46 @@ export class Modal {
     }
 
     /**
-     * Initializes the header of the modal
+     * Initializes the header of the modal.
      * @method
      * @private
+     * @param {string} [labelId='modalTitleLabel'] - The id for the title's label.
+     * @param {string} [labelText='Modal Title'] - The text to print in the title.
      */
-    #initializeHeader() {
+    #initializeHeader(labelId = 'modalTitleLabel', labelText = 'Modal Title') {
         // Create header div
         const requiredClasses = [
             'modal-header',
         ];
         const modalHeader = document.createElement('div');
         modalHeader.classList.add(requiredClasses);
-        
+
         // Close button
+        const closeButtonRequiredClasses = [
+            'close',
+        ];
+        const closeButton = document.createElement('button');
+        closeButton.classList.add(closeButtonRequiredClasses);
+        closeButton.setAttribute('data-dismiss', 'modal');
+        closeButton.setAttribute('aria-label', 'Close');
+        const closeButtonLabel = document.createElement('span');
+        closeButtonLabel.setAttribute('aria-hidden', 'true');
+        closeButtonLabel.innerText = '&times;';
+        closeButton.appendChild(closeButtonLabel);
+
         // Modal title
+        const titleRequiredClasses = [
+            'modal-title',
+        ];
+        const h4Title = document.createElement('h4');
+        h4Title.id = labelId.trim().length > 0 ? labelId.trim() : 'modalTitleLabel';
+        h4Title.innerText = labelText.trim().length > 0 ? labelText.trim() : 'Dialog Modal';
+
+        // Append elements to Header
+        modalHeader.appendChild(closeButton);
+        modalHeader.appendChild(h4Title);
+
+        this.#headerHtmlElement = modalHeader;
     }
 
     /**
