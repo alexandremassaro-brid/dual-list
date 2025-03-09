87e537aca1e88724721c897f71098d807b5259a7
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-09 10:33:08 -0300

    Criada a classe Dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,18 @@
 /**
  * @todo Implement Dropdown Class.
  */
+
+/**
+ * @classdesc A dropdown Bootstratp 3.3.7 compatible component.
+ * @class
+ * @public
+ */
+export class Dropdown {
+    /**
+     * Class constructor - Returns a Dropdown instance.
+     * @todo Implementar inicialização do elemento html.
+     */
+    constructor() {
+        // Inicializar elemento
+    }
+}
