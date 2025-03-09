25fdadc7b54183e9c793d8cacde8c20b89e98dfd
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-09 18:57:47 -0300

    Criada a classe SearchBox

--- search_box.js
+++ search_box.js
@@ -1,3 +1,10 @@
 /**
- * @todo Implement SearchBox Class.
+ * @classdesc A searchbox Bootstratp 3.3.7 compatible component.
+ * @class
+ * @public
+ * @todo Implementar método render.
+ * @todo Implementar inicialização do SearchBox.
  */
+export class SearchBox {
+    // Inicializar elementos
+}
