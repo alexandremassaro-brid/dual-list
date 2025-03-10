ec0d4e843b9b2de83b6f5c8ceb4edbc46a0f928c
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2025-03-09 23:13:28 -0300

    Criada classe DualList

--- dual_list.js
+++ dual_list.js
@@ -1,3 +1,23 @@
-/**
- * @todo Implement DualList Class.
- */
+export class DualList {
+    constructor() {
+        // Implementar inicializações dos elementos.
+        // Dual list Row div
+        // Source list
+        // Action Button
+        // Destination List
+        // Append elements to the DualList
+    }
+
+    #initializeSourceList() {
+    }
+
+    #initializeActionButtons() {
+        // Action buttons
+    #initializeDestinationList() {
+    }
+
+    render() {
+        // Implementar método render.
+    }
+    
+}
